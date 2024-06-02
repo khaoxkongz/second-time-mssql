a1b59e1b813eb543ded7570883a5caebc10c7c66
khaoxkongz <makkawal.thanapat@gmail.com>
2024-06-03 03:17:13 +0700

    chore: update db.config.js
    this commit is to update the db.config.js file to use name import instead of default import for the database connection string.

--- src/domain/configs/db.config.js
+++ src/domain/configs/db.config.js
@@ -1,11 +1,12 @@
-const constUtilities = require('../utils/constants.util');
+const { MS_SQL_USER, MS_SQL_PASSWORD } = require('../utils/constants.util');
+const { MS_SQL_SERVER, MS_SQL_PORT, MS_SQL_DATABASE } = require('../utils/constants.util');
 
 const config = {
-  user: constUtilities.MS_SQL_USER,
-  password: constUtilities.MS_SQL_PASSWORD,
-  server: constUtilities.MS_SQL_SERVER,
-  port: parseInt(constUtilities.MS_SQL_PORT, 10),
-  database: constUtilities.MS_SQL_DATABASE,
+  user: MS_SQL_USER,
+  password: MS_SQL_PASSWORD,
+  server: MS_SQL_SERVER,
+  port: parseInt(MS_SQL_PORT, 10),
+  database: MS_SQL_DATABASE,
   options: {
     encrypt: true,
     trustServerCertificate: true,

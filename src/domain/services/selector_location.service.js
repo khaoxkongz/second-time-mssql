6fd439d7bc0e90f545d6c9cb4415730e839bcbe1
khaoxkongz <makkawal.thanapat@gmail.com>
2024-06-05 22:03:48 +0700

    refactor: refactor selector_location.repo.js for improved performance and readability

--- src/domain/services/selector_location.service.js
+++ src/domain/services/selector_location.service.js
@@ -1,5 +1,3 @@
-const sql = require('mssql');
-
 const { DBO_HEALTH_ID_TABLE, SERVICE_AREA_HEALTH_COLUMN } = require('../utils/constants.util');
 const { PROVINCE_HEALTH_COLUMN } = require('../utils/constants.util');
 const { DISTRICT_HEALTH_COLUMN, SUBDISTRICT_HEALTH_COLUMN } = require('../utils/constants.util');
@@ -15,96 +13,37 @@ class ServiceSelectorLocation {
     this._repoSelectorLocation = repoSelectorLocation;
   }
 
-  async getAllProviceHealth({ areas }) {
-    const selectColumns = getSelectColumns(areas, [], []);
-
-    const query = sqlQueryDistinctPattern(selectColumns, DBO_HEALTH_ID_TABLE, {
-      whereServiceAreasArray: areas,
-      whereServiceAreaColumn: SERVICE_AREA_HEALTH_COLUMN,
-      whereProvincesArray: [],
-      whereProvinceColumn: '',
-      whereDistrictsArray: [],
-      whereDistrictColumn: '',
-    });
-
-    const inputs = buildInputs(areas, [], []);
-
+  async getAllDatas({ areas, provinces, districts, subDistricts }, { page, pageSize }) {
     try {
-      const result = await this._repoSelectorLocation.getAllProvinceHealth({ query, inputs });
-      return result;
+      const selectColumns = getSelectColumns(areas, provinces, districts);
+      const { query, queryDistinct, countQuery } = buildQueries(
+        selectColumns,
+        { areas, provinces, districts, subDistricts },
+        { page, pageSize }
+      );
+
+      const { queryResult, queryResultDistinct, totalCount } = await this._repoSelectorLocation.getAllDatas({
+        query,
+        queryDistinct,
+        countQuery,
+      });
+      return {
+        currentPage: parseInt(page, 10),
+        totalPage: getTotalPageCount(totalCount, pageSize),
+        data: queryResult,
+        distinctData: queryResultDistinct,
+      };
     } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async getAllDistrictHealth({ provinces }) {
-    const selectColumns = getSelectColumns([], provinces, []);
-
-    const query = sqlQueryDistinctPattern(selectColumns, DBO_HEALTH_ID_TABLE, {
-      whereServiceAreasArray: [],
-      whereServiceAreaColumn: '',
-      whereProvincesArray: provinces,
-      whereProvinceColumn: PROVINCE_HEALTH_COLUMN,
-      whereDistrictsArray: [],
-      whereDistrictColumn: '',
-    });
-
-    const inputs = buildInputs([], provinces, []);
-
-    try {
-      const result = await this._repoSelectorLocation.getAllDistrictHealth({ query, inputs });
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async getAllSubDistrictHealth({ districts }) {
-    const selectColumns = getSelectColumns([], [], districts);
-
-    const query = sqlQueryDistinctPattern(selectColumns, DBO_HEALTH_ID_TABLE, {
-      whereServiceAreasArray: [],
-      whereServiceAreaColumn: '',
-      whereProvincesArray: [],
-      whereProvinceColumn: '',
-      whereDistrictsArray: districts,
-      whereDistrictColumn: DISTRICT_HEALTH_COLUMN,
-    });
-
-    const inputs = buildInputs([], [], districts);
-
-    try {
-      const result = await this._repoSelectorLocation.getAllSubDistrictHealth({ query, inputs });
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async getAllDatas({ areas, provinces, districts }) {
-    const selectColumns = getSelectColumns(areas, provinces, districts);
-
-    const query = sqlQueryDistinctPattern(selectColumns, DBO_HEALTH_ID_TABLE, {
-      whereServiceAreasArray: areas,
-      whereServiceAreaColumn: SERVICE_AREA_HEALTH_COLUMN,
-      whereProvincesArray: provinces,
-      whereProvinceColumn: PROVINCE_HEALTH_COLUMN,
-      whereDistrictsArray: districts,
-      whereDistrictColumn: DISTRICT_HEALTH_COLUMN,
-    });
-
-    const inputs = buildInputs(areas, provinces, districts);
-
-    try {
-      const result = await this._repoSelectorLocation.getAllDatas({ query, inputs });
-      return result;
-    } catch (error) {
-      console.error(error);
+      return Promise.reject(error);
     }
   }
 }
 
 module.exports = { newServiceSelectorLocation };
+
+function getTotalPageCount(totalCount, pageSize) {
+  return Math.ceil(totalCount / pageSize);
+}
 
 function getSelectColumns(areas, provinces, districts) {
   const selectColumns = [];
@@ -124,69 +63,118 @@ function getSelectColumns(areas, provinces, districts) {
   return selectColumns;
 }
 
-function buildConditionClause(conditions) {
-  const conditionArray = [];
-
-  const { whereServiceAreasArray, whereProvincesArray, whereDistrictsArray } = conditions;
-  const { whereServiceAreaColumn, whereProvinceColumn, whereDistrictColumn } = conditions;
-
-  if (whereServiceAreasArray && whereServiceAreasArray.length > 0) {
-    conditionArray.push(`${whereServiceAreaColumn} IN @areas`);
-  }
-
-  if (whereProvincesArray && whereProvincesArray.length > 0) {
-    conditionArray.push(`${whereProvinceColumn} IN @provinces`);
-  }
-
-  if (whereDistrictsArray && whereDistrictsArray.length > 0) {
-    conditionArray.push(`${whereDistrictColumn} IN @districts`);
-  }
-
-  return conditionArray.join(' OR ');
+function buildBaseQuery() {
+  return `SELECT DISTINCT [organization_code], [organization_name], [service_area_health], [province_health], [district_health], [subdistrict_health], [health_office_type_health] FROM ${DBO_HEALTH_ID_TABLE}`;
 }
 
-function sqlQueryDistinctPattern(selectColumns, formTable, conditions) {
-  const selectClause = selectColumns.join(', ');
-  let query = `SELECT DISTINCT ${selectClause} FROM ${formTable}`;
+function buildWhereClause(conditions) {
+  return conditions.length > 0 ? `WHERE ${conditions.join(' AND ')}` : '';
+}
 
-  const conditionClause = buildConditionClause(conditions);
+function buildOrderByClause(orderByColumns) {
+  return orderByColumns.length > 0 ? `ORDER BY ${orderByColumns.join(', ')}` : '';
+}
 
-  if (conditionClause) {
-    query += ` WHERE ${conditionClause}`;
+function buildPaginationQuery(baseQuery, whereClause, orderByClause, { page, pageSize }) {
+  const offset = (page - 1) * pageSize;
+  return `
+    SELECT * FROM (
+      SELECT ROW_NUMBER() OVER (${orderByClause}) AS RowNum, * FROM (
+        ${baseQuery} ${whereClause}
+      ) AS SubQuery
+    ) AS PaginatedQuery
+    ${orderByClause}
+    OFFSET ${offset} ROWS FETCH NEXT ${pageSize} ROWS ONLY
+  `;
+}
+
+function buildQueries(selectColumns, { areas, provinces, districts, subDistricts }, { page, pageSize }) {
+  const { queryWithPagination, countQuery } = addConditionsToQuery(
+    { areas, provinces, districts, subDistricts },
+    { page, pageSize }
+  );
+
+  const baseQueryDistinct = buildDistinctQuery(selectColumns);
+  const queryDistinctWithConditions = addConditionToQuery(baseQueryDistinct, { areas, provinces, districts });
+
+  return {
+    query: queryWithPagination,
+    queryDistinct: queryDistinctWithConditions,
+    countQuery,
+  };
+}
+
+function addConditionsToQuery({ areas, provinces, districts, subDistricts }, { page, pageSize }) {
+  const conditions = [];
+  const orderByColumns = [];
+
+  if (areas && areas.length > 0) {
+    conditions.push(`${SERVICE_AREA_HEALTH_COLUMN} IN (${areas.map((area) => `N'${area}'`).join(', ')})`);
+    orderByColumns.push(SERVICE_AREA_HEALTH_COLUMN);
+  }
+
+  if (provinces && provinces.length > 0) {
+    conditions.push(`${PROVINCE_HEALTH_COLUMN} IN (${provinces.map((province) => `N'${province}'`).join(', ')})`);
+    orderByColumns.push(PROVINCE_HEALTH_COLUMN);
+  }
+
+  if (districts && districts.length > 0) {
+    conditions.push(`${DISTRICT_HEALTH_COLUMN} IN (${districts.map((district) => `N'${district}'`).join(', ')})`);
+    orderByColumns.push(DISTRICT_HEALTH_COLUMN);
+  }
+
+  if (subDistricts && subDistricts.length > 0) {
+    conditions.push(
+      `${SUBDISTRICT_HEALTH_COLUMN} IN (${subDistricts.map((subDistrict) => `N'${subDistrict}'`).join(', ')})`
+    );
+    orderByColumns.push(SUBDISTRICT_HEALTH_COLUMN);
+  }
+
+  const baseQuery = buildBaseQuery();
+  const whereClause = buildWhereClause(conditions);
+
+  const countQuery = `SELECT COUNT(*) AS totalCount FROM (${baseQuery} ${whereClause}) AS subQuery`;
+
+  const orderByClause = buildOrderByClause(orderByColumns);
+  const queryWithPagination = buildPaginationQuery(baseQuery, whereClause, orderByClause, { page, pageSize });
+
+  return { queryWithPagination, countQuery };
+}
+
+function addConditionToQuery(query, { areas, provinces, districts }) {
+  const conditions = [];
+
+  if (areas && areas.length > 0) {
+    conditions.push(`${SERVICE_AREA_HEALTH_COLUMN} IN ${getFormattedValues(areas)}`);
+  }
+
+  if (provinces && provinces.length > 0) {
+    conditions.push(`${PROVINCE_HEALTH_COLUMN} IN ${getFormattedValues(provinces)}`);
+  }
+
+  if (districts && districts.length > 0) {
+    conditions.push(`${DISTRICT_HEALTH_COLUMN} IN ${getFormattedValues(districts)}`);
+  }
+
+  if (conditions.length > 0) {
+    return `${query} WHERE ${conditions.join(' AND ')}`;
   }
 
   return query;
 }
 
-function buildInputs(areas, provinces, districts) {
-  const inputs = [];
-
-  if (areas && areas.length > 0) {
-    const inputAreas = { name: 'areas', type: sql.VarChar(50), value: getFormattedServiceAreas(areas) };
-    inputs.push(inputAreas);
+function buildDistinctQuery(selectColumns) {
+  if (selectColumns.includes(SUBDISTRICT_HEALTH_COLUMN)) {
+    return `SELECT DISTINCT ${SUBDISTRICT_HEALTH_COLUMN} FROM ${DBO_HEALTH_ID_TABLE}`;
+  } else if (selectColumns.includes(DISTRICT_HEALTH_COLUMN)) {
+    return `SELECT DISTINCT ${DISTRICT_HEALTH_COLUMN} FROM ${DBO_HEALTH_ID_TABLE}`;
+  } else if (selectColumns.includes(PROVINCE_HEALTH_COLUMN)) {
+    return `SELECT DISTINCT ${PROVINCE_HEALTH_COLUMN} FROM ${DBO_HEALTH_ID_TABLE}`;
   }
 
-  if (provinces && provinces.length > 0) {
-    const inputProvinces = { name: 'provinces', type: sql.VarChar(50), value: getFormattedProvinces(provinces) };
-    inputs.push(inputProvinces);
-  }
-
-  if (districts && districts.length > 0) {
-    const inputDistricts = { name: 'districts', type: sql.VarChar(50), value: getFormattedDistricts(districts) };
-    inputs.push(inputDistricts);
-  }
-
-  return inputs;
+  return '';
 }
 
-function getFormattedServiceAreas(serviceAreas) {
-  return `(${serviceAreas.map((area) => `N'${area}'`).join(', ')})`;
-}
-
-function getFormattedProvinces(provinces) {
-  return `(${provinces.map((province) => `N'${province}'`).join(', ')})`;
-}
-
-function getFormattedDistricts(districts) {
-  return `(${districts.map((district) => `N'${district}'`).join(', ')})`;
+function getFormattedValues(values) {
+  return `(${values.map((value) => `N'${value}'`).join(', ')})`;
 }
